3012d6e95b04066f733d67c6d7d744e31b51aec2
kawsar9867 <kbosuniya@gmail.com>
2026-03-16 17:51:07 +0600

    react-practice

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import ToDO from "./ToDo";
+
+function App() {
+  return <ToDO name=" Kawsar Ahamed (Eshan)" roll=" 01"></ToDO>;
+}
+export default App;
---
+++ src/ToDo.jsx
@@ -0,0 +1,8 @@
+export default function ToDO({ name, roll }) {
+  return (
+    <>
+      <h2>Name:{name}</h2>
+      <p>roll:{roll}</p>
+    </>
+  );
+}
